ebb07ae165b5632599211277788d04c579a0abea
Diego Fernando Becerra Zambrano <d.becerraz@uniandes.edu.co>
2023-04-29 22:11:53 -0500

    Punto tres

--- src/app/Vehicle/vehicle-list/vehicle-list.component.spec.ts
+++ src/app/Vehicle/vehicle-list/vehicle-list.component.spec.ts
@@ -4,14 +4,21 @@ import { By } from '@angular/platform-browser';
 import { DebugElement } from '@angular/core';
 
 import { VehicleListComponent } from './vehicle-list.component';
+import { HttpClientModule } from '@angular/common/http';
+import { VehicleService } from '../vehicle.service';
+import { Vehicle } from '../Vehicle';
+import { faker } from '@faker-js/faker';
 
 describe('VehicleListComponent', () => {
   let component: VehicleListComponent;
   let fixture: ComponentFixture<VehicleListComponent>;
+  let debug: DebugElement;
 
   beforeEach(async(() => {
     TestBed.configureTestingModule({
-      declarations: [ VehicleListComponent ]
+      imports: [HttpClientModule],
+      declarations: [ VehicleListComponent ],
+      providers: [VehicleService]
     })
     .compileComponents();
   }));
@@ -19,10 +26,35 @@ describe('VehicleListComponent', () => {
   beforeEach(() => {
     fixture = TestBed.createComponent(VehicleListComponent);
     component = fixture.componentInstance;
+
+    for(let i = 0; i < 3; i++) {
+    const vehicle = new Vehicle(
+      Number(faker.random.numeric()),
+      faker.vehicle.vehicle(),
+      faker.vehicle.manufacturer(),
+      faker.vehicle.type(),
+      Number(faker.random.numeric(1000)),
+      Number(faker.random.numeric(1000)),
+      faker.vehicle.vrm(),
+      faker.vehicle.vin(),
+    );
+    component.vehicles.push(vehicle);
+    }
+
     fixture.detectChanges();
+    debug = fixture.debugElement;
   });
 
   it('should create', () => {
     expect(component).toBeTruthy();
   });
+
+  it('should have 3 rows for vehicles an a header table', () => {
+    expect(debug.queryAll(By.css('tr'))).toHaveSize(4)
+  });
+
+  it('should thead', () => {
+    expect(debug.queryAll(By.css('thead'))).toHaveSize(1)
+  });
+
 });
--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -1,15 +1,19 @@
 import { TestBed } from '@angular/core/testing';
 import { RouterTestingModule } from '@angular/router/testing';
 import { AppComponent } from './app.component';
+import { HttpClientModule } from '@angular/common/http';
+import { VehicleListComponent } from './Vehicle/vehicle-list/vehicle-list.component';
 
 describe('AppComponent', () => {
   beforeEach(async () => {
     await TestBed.configureTestingModule({
       imports: [
-        RouterTestingModule
+        RouterTestingModule,
+        HttpClientModule
       ],
       declarations: [
-        AppComponent
+        AppComponent,
+        VehicleListComponent
       ],
     }).compileComponents();
   });
@@ -20,16 +24,5 @@ describe('AppComponent', () => {
     expect(app).toBeTruthy();
   });
 
-  it(`should have as title 'evaluacion1'`, () => {
-    const fixture = TestBed.createComponent(AppComponent);
-    const app = fixture.componentInstance;
-    expect(app.title).toEqual('evaluacion1');
-  });
 
-  it('should render title', () => {
-    const fixture = TestBed.createComponent(AppComponent);
-    fixture.detectChanges();
-    const compiled = fixture.nativeElement as HTMLElement;
-    expect(compiled.querySelector('.content span')?.textContent).toContain('evaluacion1 app is running!');
-  });
 });
